caaeac70af6103df5d237d2cda639e804986174e
tauqueerulhusain <tauqueerulhusain@gmail.com>
2024-11-17 19:33:17 +0530

    Make Testing environment

--- app/src/main/java/com/clinic/raamish/api/PatientApi.kt
+++ app/src/main/java/com/clinic/raamish/api/PatientApi.kt
@@ -22,7 +22,6 @@ import javax.inject.Singleton
 class PatientApi @Inject constructor() {
     // Dev testing database
     var database = Firebase.database("https://raameshtest-default-rtdb.asia-southeast1.firebasedatabase.app/")
-//    var database = Firebase.database("https://raamesh-31e8b-default-rtdb.firebaseio.com/") // Production database
     var patientListFLow = MutableStateFlow<List<Patient>>(emptyList())
     var listRef: DatabaseReference
     var lastAddedPatientId = MutableStateFlow<Int>(-1)
